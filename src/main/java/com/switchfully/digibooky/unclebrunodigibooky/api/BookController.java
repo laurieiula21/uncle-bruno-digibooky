62e4d8f5e0466780674fe1e098988ee0f3a7e967
brunoBaziret <bruno.baziret@gmail.com>
2021-11-29 16:25:45 +0100

    BookController

--- src/main/java/com/switchfully/digibooky/unclebrunodigibooky/api/BookController.java
+++ src/main/java/com/switchfully/digibooky/unclebrunodigibooky/api/BookController.java
@@ -1,18 +1,28 @@
 package com.switchfully.digibooky.unclebrunodigibooky.api;
 
+import com.switchfully.digibooky.unclebrunodigibooky.domain.Book;
 import com.switchfully.digibooky.unclebrunodigibooky.service.AuthorisationService;
 import com.switchfully.digibooky.unclebrunodigibooky.service.BookService;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(path = "/books")
 public class BookController {
     private final BookService bookService;
     private final AuthorisationService authorisationService;
 
-
+    @Autowired
     public BookController(BookService bookService, AuthorisationService authorisationService) {
         this.bookService = bookService;
         this.authorisationService = authorisationService;
+    }
+
+    @GetMapping(path="/{isbn}", produces = "application/json")
+    @ResponseStatus(HttpStatus.OK)
+    public Book getBook(@PathVariable("isbn") String isbn) {
+        return bookService.showDetailsOfBook(isbn);
     }
 
 
