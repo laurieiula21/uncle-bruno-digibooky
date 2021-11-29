92f86de47d99187d58fad454f76ba9fc87d2c57b
Dries Verreydt <johndoe@example.com>
2021-11-29 14:24:46 +0100

    first commit project setup

---
+++ src/main/java/com/switchfully/digibooky/unclebrunodigibooky/UncleBrunoDigibookyApplication.java
@@ -0,0 +1,13 @@
+package com.switchfully.digibooky.unclebrunodigibooky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UncleBrunoDigibookyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UncleBrunoDigibookyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/switchfully/digibooky/unclebrunodigibooky/UncleBrunoDigibookyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.switchfully.digibooky.unclebrunodigibooky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UncleBrunoDigibookyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
